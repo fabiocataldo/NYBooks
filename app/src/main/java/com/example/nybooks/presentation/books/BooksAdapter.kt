1e4560067d16e5f1c48b3164849e5cb8ddb9fcbd
Fabio Cataldo <fabiocataldo@gmail.com>
2020-02-11 18:00:24 +0000

    Creating Adapter

---
+++ app/src/main/java/com/example/nybooks/presentation/books/BooksAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.nybooks.presentation.books
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class BooksAdapter: RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BooksViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: BooksViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class BooksViewHolder: RecyclerView.ViewHolder(){}
+
+    }
